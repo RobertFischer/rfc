55987551e3e54a1d990d8af8ddf68855ecb51cfd
Robert Fischer <robert+github@getbeewell.com>
2017-09-19 15:55:36 -0400

    Added in the logic so that it prints out the variables that it is looking for

--- src/RFC/Env.hs
+++ src/RFC/Env.hs
@@ -1,10 +1,34 @@
 module RFC.Env
-  ( checkDevelopment
+  ( isDevelopment
+  , readEnvironment
+  , readGoogleMapsAPIKey
   ) where
 
 import RFC.Prelude
-import Data.Maybe (maybe)
+import Control.Logger.Simple (logInfo)
 import System.Environment (lookupEnv)
 
-checkDevelopment :: IO Bool
-checkDevelopment = maybe True ((/=) "development") <$> lookupEnv "ENV"
+-- TODO Create a Monad that only logs reading the env var once, and reads all the environment variables at once, and is pure.
+
+readEnv :: String -> Maybe String -> IO String
+readEnv envKey defaultValue = do
+  foundValue <- lookupEnv envKey
+  case foundValue of
+    Nothing ->
+      case defaultValue of
+        Nothing -> fail $ "No value of " ++ (show envKey) ++ " environment variable, and no default configured."
+        (Just s) -> return s
+    (Just s) -> do
+      logInfo (cs $ "Reading Env Var. Key=" ++ (show envKey) ++ " Value=" ++ (show s))
+      return s
+
+isDevelopment :: IO Bool
+isDevelopment = ((==) "development") <$> readEnvironment
+
+readEnvironment :: IO String
+readEnvironment = readEnv "ENV" defaultEnv
+  where
+    defaultEnv = Just "development"
+
+readGoogleMapsAPIKey :: IO String
+readGoogleMapsAPIKey = readEnv "GMAPS_API_KEY" Nothing
