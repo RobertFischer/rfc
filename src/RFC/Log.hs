9988b2e9caebc67062589c67ba4065c2c2eb7ee4
Robert Fischer <robert+github@getbeewell.com>
2017-09-19 15:55:02 -0400

    Set the buffering on stderr for logging

--- src/RFC/Log.hs
+++ src/RFC/Log.hs
@@ -6,7 +6,12 @@ module RFC.Log
 import RFC.Prelude
 import Control.Logger.Simple (pureDebug, pureInfo, pureWarn, pureError, logDebug, logInfo, logWarn, logError)
 import Control.Logger.Simple as Log
+import System.IO (stderr, hSetBuffering, BufferMode(..))
 
 withLogging :: IO a -> IO a
-withLogging = Log.withGlobalLogging Log.LogConfig { Log.lc_file = Nothing, Log.lc_stderr = True }
+withLogging action = do
+  hSetBuffering stderr LineBuffering
+  Log.withGlobalLogging logConfig action
+  where
+    logConfig = Log.LogConfig { Log.lc_file = Nothing, Log.lc_stderr = True }
 
