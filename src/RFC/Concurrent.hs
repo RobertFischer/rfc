8539ebd3b6d4fe20e7f94b048a85f6a24a66adc2
Robert Fischer <robert+github@getbeewell.com>
2017-11-06 09:44:31 -0500

    Change to lifted versions of the async resources

--- src/RFC/Concurrent.hs
+++ src/RFC/Concurrent.hs
@@ -1,22 +1,22 @@
 module RFC.Concurrent
   ( module RFC.Concurrent
-  , module Control.Concurrent.Async
+  , module Control.Concurrent.Async.Lifted
   ) where
 
 import RFC.Prelude hiding (mapConcurrently)
-import Control.Concurrent.Async (mapConcurrently, mapConcurrently_)
+import Control.Concurrent.Async.Lifted (mapConcurrently, mapConcurrently_)
 
 -- |Executes all the IO actions simultaneously and returns the original data structure with the arguments replaced
 --  by the results of the execution.
-doConcurrently :: (Traversable t) => t (IO a) -> IO (t a)
+doConcurrently :: (Traversable t, MonadBaseControl IO m) => t (m a) -> m (t a)
 doConcurrently = mapConcurrently id
 
 -- |Executes all the IO actions simulataneously and discards the results.
-doConcurrently_ :: (Foldable f) => f (IO a) -> IO ()
+doConcurrently_ :: (Foldable f, MonadBaseControl IO m) => f (m a) -> m ()
 doConcurrently_ = mapConcurrently_ id
 
 -- |Executes all the IO actions simultaneously, feeds them into the filter function, and then
 -- filters the results.
-filterConcurrently :: (a -> Bool) -> [IO a] -> IO [a] -- TODO Seems like we should be able to do this with Foldable/Traversable
+filterConcurrently :: (MonadBaseControl IO m) => (a -> Bool) -> [m a] -> m [a] -- TODO Seems like we should be able to do this with Foldable/Traversable
 filterConcurrently filterFunc actions =
     filter filterFunc <$> doConcurrently actions
