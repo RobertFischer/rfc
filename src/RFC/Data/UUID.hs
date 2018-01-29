3b5558a65668e117161337a6ba233b98fea27712
Robert Fischer <robert+github@getbeewell.com>
2018-01-29 16:13:03 -0500

    Allow FormUrlEncoded

--- src/RFC/Data/UUID.hs
+++ src/RFC/Data/UUID.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE CPP                   #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE TypeSynonymInstances  #-}
 
 module RFC.Data.UUID
@@ -13,11 +14,19 @@ import           RFC.String
 import           Data.UUID.Types
 import qualified Data.UUID.Types                    as UUID
 
+#if MIN_VERSION_aeson(1,1,0)
+-- UUID has ToJSON and FromJSON
+#else
+import           Data.Aeson.Types
+#endif
+
 #ifndef GHCJS_BROWSER
+
 import           Database.PostgreSQL.Simple.FromRow
 import           Database.PostgreSQL.Simple.ToRow
 import           Database.PostgreSQL.Simple.Types   (Only (..))
 #endif
+
 
 #ifndef GHCJS_BROWSER
 instance FromRow UUID where
@@ -27,6 +36,23 @@ instance FromRow UUID where
 
 instance ToRow UUID where
   toRow id = toRow $ Only id
+#endif
+
+#if MIN_VERSION_aeson(1,1,0)
+-- UUID has ToJSON and FromJSON
+#else
+
+instance ToJSON UUID where
+  toJSON = toJSON . UUID.toText
+
+instance FromJSON UUID where
+  parseJSON (String txt) =
+    case UUID.fromText txt of
+      Nothing   -> fail $ "Invalid UUID: " ++ cs txt
+      Just uuid -> return uuid
+
+  parseJSON invalid = typeMismatch "UUID" invalid
+
 #endif
 
 instance ConvertibleStrings UUID String where
