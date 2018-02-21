e0708eda69bac6e17d8380bd4fef0bd942c4f468
Robert Fischer <robert+github@getbeewell.com>
2018-02-21 14:44:01 -0500

    Export routing

--- src/RFC/Miso.hs
+++ src/RFC/Miso.hs
@@ -5,8 +5,10 @@ module RFC.Miso
   ( module RFC.Miso.String
   , module RFC.Miso.XHR
   , module RFC.Miso.Inject
+  , module RFC.Miso.Routing
   ) where
 
 import           RFC.Miso.Inject
+import           RFC.Miso.Routing
 import           RFC.Miso.String
 import           RFC.Miso.XHR
