21d328535bedfdbe053d083c1d7d59c1a178ecaa
Robert Fischer <robert+github@getbeewell.com>
2018-02-28 18:07:25 -0500

    Update attempting to use lenses

--- src/RFC/Env.hs
+++ src/RFC/Env.hs
@@ -1,115 +1,39 @@
 {-# LANGUAGE CPP #-}
 
 module RFC.Env
-  ( readGoogleMapsAPIKey
-  , isDevelopment
+  ( isDevelopment
   , readEnvironment
-  , readPsqlConnectInfo
-  , readRedisConnectInfo
   , readHost
   , readPort
   ) where
 
-import           Data.Word                  (Word16)
+import           Control.Applicative
+import           Control.Exception
+import           Control.Monad
+import           Data.Either
+import           Data.Word
 import           Database.PostgreSQL.Simple as Psql
 import           Database.Redis             as Redis
+import           GHC.TypeLits               (Symbol)
 import           RFC.Prelude
-import           System.Environment         (lookupEnv)
+import           System.Environment
 
 -- TODO Create a Monad that only logs reading the env var once, and reads all the environment variables at once, and is pure.
 
-readEnv :: (MonadIO m) => String -> Maybe String -> m String
-readEnv envKey defaultValue = do
-  foundValue <- liftIO $ lookupEnv envKey
-  case foundValue of
-    Nothing ->
-      case defaultValue of
-        Nothing -> fail $ "No value of " ++ (show envKey) ++ " environment variable, and no default configured."
-        (Just s) -> do
-          return s
-    (Just s) -> do
-      return s
+isDevelopment :: (MonadIO m) => m Bool
+isDevelopment = fmap ((==) "development") readEnvironment
 
-isDevelopment :: (MonadIO m) =>  m Bool
-isDevelopment = ((==) "development") <$> readEnvironment
-
-forDevOnly :: (MonadIO m) => String -> m (Maybe String)
+forDevOnly :: (MonadIO m) => a -> m (Maybe a)
+#ifdef DEVELOPMENT
+forDevOnly defaultValue = Just defaultValue
+#else
 forDevOnly defaultValue = do
   isDev <- isDevelopment
-  return $ if isDev then
-    Just defaultValue
-  else
-    Nothing
+  return $ isDev <|> Just defaultValue
+#endif
 
 readEnvironment :: (MonadIO m) => m String
-readEnvironment = readEnv "ENV" defaultEnv
-  where
-    defaultEnv = Just "development"
-
-readGoogleMapsAPIKey :: (MonadIO m) => m String
-readGoogleMapsAPIKey = readEnv "GMAPS_API_KEY" Nothing
-
-readPsqlUser :: (MonadIO m) => m String
-readPsqlUser = do
-  projectThunk <- readAppSlug >>= forDevOnly
-  readEnv "PSQL_USERNAME" $ projectThunk
-
-readPsqlPassword :: (MonadIO m) => m String
-readPsqlPassword = do
-  projectThunk <- readAppSlug >>= forDevOnly
-  readEnv "PSQL_PASSWORD" projectThunk
-
-readPsqlDatabase :: (MonadIO m) => m String
-readPsqlDatabase = do
-  projectThunk <- readAppSlug >>= forDevOnly
-  readEnv "PSQL_DATABASE" projectThunk
-
-readPsqlHost :: (MonadIO m) => m String
-readPsqlHost = readEnv "PSQL_HOST" $ Just $ Psql.connectHost Psql.defaultConnectInfo
-
-readPsqlPort :: (MonadIO m) => m Word16
-readPsqlPort = do
-  result <- readEnv "PSQL_PORT" $ Just $ show $ Psql.connectPort Psql.defaultConnectInfo
-  return $ read result
-
-readPsqlConnectInfo :: (MonadIO m) => m Psql.ConnectInfo
-readPsqlConnectInfo =
-  Psql.ConnectInfo
-    <$> readPsqlHost
-    <*> readPsqlPort
-    <*> readPsqlUser
-    <*> readPsqlPassword
-    <*> readPsqlDatabase
-
-readRedisConnectInfo :: (MonadIO m) => m Redis.ConnectInfo
-readRedisConnectInfo =
-  Redis.ConnInfo
-    <$> readRedisHost
-    <*> readRedisPort
-    <*> readRedisPassword
-    <*> readRedisDbNumber
-    <*> (return $ Redis.connectMaxConnections Redis.defaultConnectInfo)
-    <*> (return $ Redis.connectMaxIdleTime Redis.defaultConnectInfo)
-    <*> (return $ Just 10)
-
-readRedisHost :: (MonadIO m) => m Redis.HostName
-readRedisHost = readEnv "REDIS_HOST" $ Just $ Redis.connectHost Redis.defaultConnectInfo
-
-readRedisPort :: (MonadIO m) => m Redis.PortID
-readRedisPort = do
-  result <- readEnv "REDIS_PORT" $ Just "6379" -- Default Redis port
-  return $ PortNumber $ read result
-
-readRedisPassword :: (MonadIO m) => m (Maybe ByteString)
-readRedisPassword = do
-  result <- readEnv "REDIS_PASSWORD" $ Just ""
-  return $
-    case null result of
-      True  -> Nothing
-      False -> Just $ cs result
-
-readRedisDbNumber :: (MonadIO m) => m Integer
-readRedisDbNumber = read <$> readEnv "REDIS_DATABASE" (Just $ show $ Redis.connectDatabase Redis.defaultConnectInfo)
+readEnvironment = readEnv "ENV" "development"
 
 readAppSlug :: (MonadIO m) => m String
 readAppSlug = readEnv "APP_SLUG" Nothing
@@ -124,3 +48,6 @@ readPort devPort = do
   result <- readEnv "PORT" defaultPort
   return $ (read result :: Word16)
 
+readEnv name defaultValue = liftIO $ do
+  result <- lookupEnv name
+  return $ result <|> defaultValue
--- src/RFC/Miso/Component.hs
+++ src/RFC/Miso/Component.hs
@@ -1,22 +1,13 @@
-{-# LANGUAGE AllowAmbiguousTypes       #-}
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE FlexibleContexts          #-}
-{-# LANGUAGE GADTs                     #-}
-{-# LANGUAGE LambdaCase                #-}
-{-# LANGUAGE MultiParamTypeClasses     #-}
-{-# LANGUAGE RankNTypes                #-}
-{-# LANGUAGE ScopedTypeVariables       #-}
-{-# LANGUAGE TypeFamilies              #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE LambdaCase            #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeFamilies          #-}
 
 {-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
 
 module RFC.Miso.Component
   ( Component(..)
-  , ComponentEmbed(..)
-  , ComponentContainer(..)
-  , ComponentProxy(..)
-  , EmbeddedComponent(..)
-  , embeddedComponentToProxy
   , viewComponent
   , updateComponents
   , wrappedView
@@ -24,6 +15,8 @@ module RFC.Miso.Component
   , wrapEffect
   ) where
 
+import           Control.Lens    hiding (view)
+import qualified Control.Lens    as Lens
 import           Data.Proxy
 import           Miso.Effect
 import           Miso.Html       (View (..))
@@ -49,73 +42,56 @@ class (Eq model) => Component model where
   transition action = toTransition $ update action
   {-# INLINE transition #-} -- Only inlines if instance uses default method
 
-class (Component model, Component parentModel) => ComponentLens parentModel model where
-  --initArgsLens :: Lens' (InitArgs parentModel) (InitArgs model)
-  unwrapInitArgs :: Proxy model -> InitArgs parentModel -> InitArgs model
-  wrapAction     :: model -> Action model -> Action parentModel
-  unwrapAction   :: model -> Action parentModel -> Maybe (Action model)
-  wrapModel      :: model -> parentModel -> parentModel
-  wrapModelPxy   :: Proxy model -> model -> parentModel -> parentModel  -- ^ For cases when we need to help type inference
-  wrapModelPxy _ = wrapModel
-  unwrapModel    :: Proxy model -> parentModel -> model
+class (Component model, Component parentModel) => ChildComponent parentModel model where
+  childInitArgs  :: Getter (InitArgs parentModel) (InitArgs model)
+  childAction    :: Lens' (Action parentModel) (Maybe (Action model))
+  parentAction   :: Getter (Action model) (Action parentModel)
+  childModel     :: Lens' parentModel model
 
-data EmbeddedComponent parentModel where
-  EmbeddedComponentProxy :: (ComponentEmbed parentModel model) => Proxy model -> EmbeddedComponent parentModel
+wrapAction :: (ChildComponent parentModel model) => Action model -> Action parentModel
+wrapAction action = action ^. parentAction
+{-# INLINE wrapAction #-}
 
-embeddedComponentToProxy :: EmbeddedComponent parentModel -> (ComponentEmbed parentModel model) => Proxy model
-embeddedComponentToProxy (EmbeddedComponentProxy pxy) = pxy
-{-# INLINE embeddedComponentToProxy #-}
+wrapActionF :: (ChildComponent parentModel model, Functor f) => f (Action model) -> f (Action parentModel)
+wrapActionF = mapped %~ wrapAction
+{-# INLINE wrapActionF #-}
+{-# SPECIALISE INLINE wrapActionF :: (ChildComponent parentModel model) => [Action model] -> [Action parentModel] #-}
+{-# SPECIALISE INLINE wrapActionF :: (ChildComponent parentModel model) => IO (Action model) -> IO (Action parentModel) #-}
 
-embeddedComponentToModel :: (ComponentEmbed parentModel model) => EmbeddedComponent parentModel -> parentModel -> model
-embeddedComponentToModel = unwrapModel . embeddedComponentToProxy
-{-# INLINE embeddedComponentToModel #-}
+wrapActionF2 :: (ChildComponent parentModel model, Functor f1, Functor f2) => f1 (f2 (Action model)) -> f1 (f2 (Action parentModel))
+wrapActionF2 = mapped %~ wrapActionF
+{-# INLINE wrapActionF2 #-}
+{-# SPECIALISE INLINE wrapActionF2 :: (ChildComponent parentModel model) =>  IO [Action model]  ->  IO [Action parentModel]  #-}
+{-# SPECIALISE INLINE wrapActionF2 :: (ChildComponent parentModel model) => [IO (Action model)] -> [IO (Action parentModel)] #-}
 
-wrapIOActions :: (ComponentEmbed parentModel model) => model -> [IO (Action model)] -> [IO (Action parentModel)]
-wrapIOActions childModel = map (fmap $ wrapAction childModel)
-{-# INLINE wrapIOActions #-}
-
-wrapEffect :: (ComponentEmbed parentModel model) => Effect (Action model) model -> parentModel -> Effect (Action parentModel) parentModel
-wrapEffect (Effect childModel childIOs) parentModel =
+wrapEffect :: (ChildComponent parentModel model) =>
+  Effect (Action model) model -> parentModel -> Effect (Action parentModel) parentModel
+wrapEffect (Effect model childIOs) parentModel =
   Effect
-    (wrapModel childModel parentModel)
-    (wrapIOActions childModel childIOs)
+    (parentModel & childModel .~ model )
+    (wrapActionF2 childIOs)
 {-# INLINE wrapEffect #-}
 
-wrappedUpdate :: (ComponentEmbed parentModel model) =>
-  Proxy model -> Action parentModel -> parentModel -> Effect (Action parentModel) parentModel
-wrappedUpdate pxy parentAction parentModel = fromMaybe (noEff parentModel) $ do
-  let model = unwrapModel pxy parentModel
-  action <- unwrapAction model parentAction
+wrappedUpdate :: (ChildComponent parentModel model) =>
+  Proxy model ->
+  Action parentModel ->
+  parentModel ->
+  Effect (Action parentModel) parentModel
+wrappedUpdate _ parentAction parentModel = fromMaybe (noEff parentModel) $ do
+  action <- parentAction ^. childAction :: Maybe (Action model)
+  let model = parentModel ^. childModel :: model
   return $ wrapEffect (update action model) parentModel
 {-# INLINABLE wrappedUpdate #-}
 
-wrappedView :: (ComponentEmbed parentModel model) => parentModel -> Proxy model -> View (Action parentModel)
-wrappedView parentModel pxy =
-  maybe
-    (Html.text $ cs "")
-    (\model -> fmap (wrapAction model) $ view model)
-    (unwrapModel pxy parentModel)
+wrappedView :: (ChildComponent parentModel model) => parentModel -> Proxy model -> View (Action parentModel)
+wrappedView parentModel _ =
+    parentModel ^. childModel & view & wrapActionF
+  where
+    doView :: model -> View (Action model)
+    doView = view
 {-# INLINABLE wrappedView #-}
 
-class ComponentContainer parentModel where
-  components :: Proxy parentModel -> [EmbeddedComponent parentModel]
-
-viewComponent :: (ComponentEmbed parentModel model) => parentModel -> Proxy model -> View (Action parentModel)
+viewComponent :: (ChildComponent parentModel model) => parentModel -> Proxy model -> View (Action parentModel)
 viewComponent = wrappedView
 {-# INLINE viewComponent #-}
 
-updateComponents ::
-  (ComponentContainer parentModel, Component parentModel) =>
-  Action parentModel -> parentModel -> Effect (Action parentModel) parentModel
-updateComponents action startingParentModel = foldr foldImpl initialEffect comps
-  where
-    parentPxy = Proxy :: Proxy parentModel
-    comps = components parentPxy
-    initialEffect = noEff startingParentModel
-    foldImpl component (Effect parentModel ios) =
-      Effect newParentModel (ios ++ newIos)
-        where
-          pxy = embeddedComponentToProxy component
-          Effect newParentModel newIos = wrappedUpdate pxy action parentModel
-
-{-# INLINABLE updateComponents #-}
