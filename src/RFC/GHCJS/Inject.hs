3fa94e200a0f0db9175093c69ecf64fc83e133ea
Robert Fischer <robert+github@getbeewell.com>
2018-03-08 11:15:24 -0500

    Add in stylesheet

--- src/RFC/GHCJS/Inject.hs
+++ src/RFC/GHCJS/Inject.hs
@@ -5,6 +5,7 @@
 module RFC.GHCJS.Inject
   ( injectJS
   , injectCSS
+  , injectStylesheet
   , injectPreconnectHint
   , injectPreconnectHintURI
   , writeDocumentTitle
@@ -14,38 +15,48 @@ import           Network.URI
 import           RFC.Miso.String
 import           RFC.Prelude
 
+-- | Injects a String of CSS declarations as an inline <style type="text/css"> element.
+injectStylesheet :: String -> IO ()
+injectStylesheet styling = injectStylesheet' . cs
+{-# INLINE injectStylesheet #-}
+
+-- | Injects a reference to a JS file as a <script type="text/javascript"> element.
 injectJS :: String -> IO ()
 injectJS = injectJS' . cs
-{-# INLINABLE injectJS #-}
+{-# INLINE injectJS #-}
 
+-- | Injects a reference to a CSS file as a <link rel="stylesheet"> element.
 injectCSS :: String -> IO ()
 injectCSS = injectCSS' . cs
-{-# INLINABLE injectCSS #-}
+{-# INLINE injectCSS #-}
 
+-- | Injects a preconnect hint as a <link rel="preconnect"> element.
 injectPreconnectHint :: String -> IO ()
 injectPreconnectHint str =
   case parseURI str of
     Nothing  -> injectPreconnectHint' str
     Just uri -> injectPreconnectHintURI uri
-{-# INLINABLE injectPreconnectHint #-}
+{-# INLINE injectPreconnectHint #-}
 
+-- | Injects a preconnect hint as a <link rel="preconnect"> element.
 injectPreconnectHintURI :: URI -> IO ()
 injectPreconnectHintURI uri =
   case uriAuthority uri of
     Nothing -> return ()
     Just URIAuth{uriRegName} ->
       injectPreconnectHint uriRegName
-{-# INLINABLE injectPreconnectHintURI #-}
+{-# INLINE injectPreconnectHintURI #-}
 
 injectPreconnectHint' :: String -> IO ()
 injectPreconnectHint' ""          = return ()
 injectPreconnectHint' "localhost" = return ()
 injectPreconnectHint' "127.0.0.1" = return ()
 injectPreconnectHint' str         = injectPreconnectHint'' . cs $ str
+{-# INLINE injectPreconnectHint' #-}
 
 foreign import javascript safe
-	"if(document.title !== $1) { document.title = $1 };"
-	writeDocumentTitle :: MisoString -> IO ()
+  "if(document.title !== $1) { document.title = $1 };"
+  writeDocumentTitle :: MisoString -> IO ()
 
 foreign import javascript safe
   "var script=document.createElement('link');script.rel='preconnect';script.crossorigin=true;script.href='https://' + $1;document.getElementsByTagName('head')[0].appendChild(script);"
@@ -58,3 +69,7 @@ foreign import javascript safe
 foreign import javascript safe
   "var ss=document.createElement('link');ss.rel='stylesheet';ss.href=$1;ss.type='text/css';document.getElementsByTagName('head')[0].appendChild(ss);"
   injectCSS' :: MisoString -> IO ()
+
+foreign import javascript safe
+  "var ss=document.createElement('style');ss.type='text/css';ss.innerHTML=$1;document.getElementsByTagName('head')[0].appendChild(ss);"
+  injectStylesheet' :: MisoString -> IO ()
