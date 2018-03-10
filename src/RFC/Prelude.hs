6b8d5d2bd350fe5dbc09572e8ed948a1d08042df
Robert Fischer <robert+github@getbeewell.com>
2018-03-10 12:11:22 -0500

    Broaden safeHead and add in some more MonadFail instances

--- src/RFC/Prelude.hs
+++ src/RFC/Prelude.hs
@@ -1,3 +1,10 @@
+{-# LANGUAGE CPP                        #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE UndecidableInstances       #-}
+
 module RFC.Prelude
   ( module RFC.Prelude
   , module RFC.Data.UUID
@@ -13,7 +20,6 @@ module RFC.Prelude
   , module Data.Default
   , module ClassyPrelude
   , module Control.Monad.Trans.Control
-  , module Control.Monad.IO.Unlift
   , module RFC.String
   , module Data.Word
   , module Data.Semigroup
@@ -25,13 +31,13 @@ module RFC.Prelude
 import           ClassyPrelude               hiding (Day, fail, unpack)
 import           Control.Monad               (forever, void, (<=<), (>=>))
 import           Control.Monad.Fail          (MonadFail, fail)
-import           Control.Monad.IO.Unlift
 import           Control.Monad.Trans.Control
 import           Data.Bifoldable
 import           Data.Bifunctor
 import           Data.Bitraversable
 import           Data.Char                   as Char
 import           Data.Default
+import qualified Data.Foldable               as Foldable
 import           Data.Function               ((&))
 import qualified Data.List                   as List
 import           Data.Semigroup
@@ -39,26 +45,64 @@ import           Data.Time.Clock
 import           Data.Time.Units
 import           Data.Typeable               (TypeRep, typeOf)
 import           Data.Word                   (Word16)
+import           GHC.Conc.Sync
 import           GHC.Generics                (Generic)
 import           RFC.Data.UUID               (UUID)
 import           RFC.String
 import           Text.Read                   (Read, read)
 import           UnliftIO
+#ifdef VERSION_exceptions
+import           Control.Monad.Catch
+#endif
 
 charIsUpper :: Char -> Bool
 charIsUpper = Char.isUpper
+{-# INLINE charIsUpper #-}
 
 charIsLower :: Char -> Bool
 charIsLower = Char.isLower
+{-# INLINE charIsLower #-}
 
 uniq :: (Eq a) => [a] -> [a]
 uniq = List.nub
+{-# INLINE uniq #-}
 
-safeHead :: [a] -> Maybe a
-safeHead []    = Nothing
-safeHead (x:_) = Just x
+safeHead :: (MonadFail m, Foldable f) => f a -> m a
+safeHead xs =
+  case Foldable.toList xs of
+    []    -> fail "Attempted to head an empty list"
+    (x:_) -> return x
+{-# INLINE safeHead #-}
+{-# SPECIALIZE INLINE safeHead :: [a] -> Maybe a #-}
 
 foldl :: MonoFoldable mono => (a -> Element mono -> a) -> a -> mono -> a
 foldl = foldl'
+{-# INLINE foldl #-}
 
 type Boolean = Bool -- I keep forgetting which Haskell uses....
+
+newtype Failed = Failed String
+  deriving (Show, Eq, Ord, Generic, Typeable)
+instance Exception Failed
+
+instance {-# OVERLAPPABLE #-} (Monad m, MonadIO m) => MonadFail m where
+  fail = throwIO . Failed
+  {-# INLINE fail #-}
+
+#ifdef VERSION_exceptions
+instance {-# OVERLAPPABLE #-} MonadThrow m => MonadFail m where
+  fail = throwM . Failed
+  {-# INLINE fail #-}
+#endif
+
+instance {-# OVERLAPPING #-} MonadFail STM where
+  fail _ = retry
+  {-# INLINE fail #-}
+
+instance {-# OVERLAPPING #-} MonadFail (Either String) where
+  fail = Left
+  {-# INLINE fail #-}
+
+instance {-# OVERLAPPING #-} MonadFail Option where
+  fail _ = Option Nothing
+  {-# INLINE fail #-}
