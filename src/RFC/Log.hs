d0dce42193b8c67ca634efea91cbc486aac5e51d
Robert Fischer <robert+github@getbeewell.com>
2018-04-27 10:12:31 -0400

    Add the timestamp into the log file name

--- src/RFC/Log.hs
+++ src/RFC/Log.hs
@@ -9,21 +9,24 @@ import           Control.Logger.Simple (logDebug, logError, logInfo, logWarn,
                                         pureDebug, pureError, pureInfo,
                                         pureWarn)
 import qualified Control.Logger.Simple as Log
+import           Data.Time.Format
+import           Data.Time.LocalTime
 import           RFC.Env               as Env
 import           RFC.Prelude
 import           System.IO             (BufferMode (..), stderr)
 
 withLogging :: (MonadUnliftIO m, MonadFail m) => m a -> m a
 withLogging action = do
-  appSlug <- Env.readAppSlug
   hSetBuffering stderr LineBuffering
   ioAction <- toIO action
-  liftIO $ Log.withGlobalLogging (logConfig appSlug) ioAction
+  appSlug <- Env.readAppSlug
+  timestamp <- formatTime defaultTimeLocale "%_Y%m%d.%H%M.%S%Q.%Z" <$> liftIO getZonedTime
+  liftIO $ Log.withGlobalLogging (logConfig appSlug timestamp) ioAction
   where
-    logConfig appSlug =
+    logConfig appSlug timestamp =
       if Env.isDevelopment then
           Log.LogConfig { Log.lc_file = Nothing, Log.lc_stderr = True }
       else
-          Log.LogConfig { Log.lc_file = Just ("./log/" <> appSlug <> ".log"), Log.lc_stderr = False }
+          Log.LogConfig { Log.lc_file = Just ("./log/" <> appSlug <> "." <> timestamp <> ".log"), Log.lc_stderr = False }
 
 
